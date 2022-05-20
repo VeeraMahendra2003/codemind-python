d591437a920c4ac04c5aa8b67260fec010b0bc36
VeeraMahendra2003 <97424987+VeeraMahendra2003@users.noreply.github.com>
2022-05-20 11:30:58 +0530

    t=int(input()) for i in range(t):     a=int(input())     c=0     for n in range(a,1,-1):         for j in range(2,int(n**0.5)+1):             if n%j==0:                 break         else:             z=n             break     b=a     while a:         for j in range(2,int(a**0.5)+1):             if a%j==0:                 break         else:             x=a             break         a+=1     if abs(b-n)>abs(a-b):         print(x)     else:         print(z)

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,22 +1,25 @@
 t=int(input())
-for j in range(1,t+1):
+for i in range(t):
     a=int(input())
+    c=0
     for n in range(a,1,-1):
-        for i in range(2,int(a**0.5)+1):
-            if n%i==0:
+        for j in range(2,int(n**0.5)+1):
+            if n%j==0:
                 break
         else:
+            z=n
             break
     b=a
     while a:
-        for i in range(2,int(a**0.5)+1):
-            if a%i==0:
+        for j in range(2,int(a**0.5)+1):
+            if a%j==0:
                 break
-        
         else:
+            x=a
             break
         a+=1
     if abs(b-n)>abs(a-b):
-        print(a)
+        print(x)
     else:
-        print(n)
+        print(z)
+        
