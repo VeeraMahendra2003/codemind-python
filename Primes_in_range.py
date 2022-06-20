fd0d2d83f60077234527454c1c073ac4ac381a73
VeeraMahendra2003 <97424987+VeeraMahendra2003@users.noreply.github.com>
2022-06-20 20:07:35 +0530

    def prime(x):     if x==0 or x==1:         return False     for i in range(2,int(x**0.5)+1):         if x%i==0:             return False     else:         return True n=int(input()) m=int(input()) c=0 for i in range(n,m+1):     if prime(i):         c+=1 print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,12 +1,11 @@
 def prime(x):
-    if x==1 or x==0:
+    if x==0 or x==1:
         return False
+    for i in range(2,int(x**0.5)+1):
+        if x%i==0:
+            return False
     else:
-        for i in range(2,int(x**0.5)+1):
-            if x%i==0:
-                return False
-        else:
-            return True
+        return True
 n=int(input())
 m=int(input())
 c=0
