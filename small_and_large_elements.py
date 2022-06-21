d7d6b6d64f3595b63f192ce55c3e3c6ed5a81857
VeeraMahendra2003 <97424987+VeeraMahendra2003@users.noreply.github.com>
2022-06-21 15:58:53 +0530

    n=input().split(" ") k=[] x=n[0] k.append(min(x)) y=n[-1] k.append(max(y)) print(*k)

--- small_and_large_elements.py
+++ small_and_large_elements.py
@@ -1,7 +1,7 @@
-n=input()
-c=n.split()
-d=c[0]
-s=c[::-1]
-t=s[0]
-print(min(d),end=' ')
-print(max(t),end='')
+n=input().split(" ")
+k=[]
+x=n[0]
+k.append(min(x))
+y=n[-1]
+k.append(max(y))
+print(*k)
