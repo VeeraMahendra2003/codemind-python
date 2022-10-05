e3c637334d38d419693e9a6737cb163953046a5d
VeeraMahendra2003 <97424987+VeeraMahendra2003@users.noreply.github.com>
2022-10-05 08:08:14 +0530

    def plusOne(a):     n = len(a) - 1     while a[n] == 9:         a[n] = 0         n = n - 1     if n < 0:         a = [1] + a     else:         a[n] = a[n] + 1     return a
    t=int(input())
    a=list(map(int,input().split()))
    print(*plusOne(a))

---
+++ Plus_One.py
@@ -0,0 +1,14 @@
+def plusOne(a):
+    n = len(a) - 1
+    while a[n] == 9:
+        a[n] = 0
+        n = n - 1
+    if n < 0:
+        a = [1] + a
+    else:
+        a[n] = a[n] + 1
+    return a
+
+t=int(input())
+a=list(map(int,input().split()))
+print(*plusOne(a))
